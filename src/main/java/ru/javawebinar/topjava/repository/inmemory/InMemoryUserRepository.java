ffb3258dd9ae020c48e179ab2c3130f50bae52d4
Kurururo <56359915+Kururuzo@users.noreply.github.com>
2020-02-18 20:20:38 +0300

    HW 02 refactor UserUtil

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -3,13 +3,16 @@ package ru.javawebinar.topjava.repository.inmemory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.AbstractBaseEntity;
+import ru.javawebinar.topjava.model.AbstractNamedEntity;
+import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
-import ru.javawebinar.topjava.util.UsersUtil;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @Repository
 public class InMemoryUserRepository implements UserRepository {
@@ -19,7 +22,13 @@ public class InMemoryUserRepository implements UserRepository {
     private AtomicInteger counter = new AtomicInteger(0);
 
     {
-        UsersUtil.USERS.forEach(this::save);
+        List <User> USERS = Arrays.asList(
+                new User(null, "Vasya", "vasya@mail.ru", "vasyaPassword", Role.ROLE_USER),
+                new User(null, "Petya", "petya@mail.ru", "petyaPassword", Role.ROLE_USER),
+                new User(null, "Kolya", "kolya@mail.ru", "kolyaPassword", Role.ROLE_ADMIN),
+                new User(null, "Pupkin", "pupkin@mail.com", "pupkinPassword", Role.ROLE_ADMIN, Role.ROLE_USER)
+        );
+        USERS.forEach(this::save);
     }
 
     @Override
@@ -48,7 +57,10 @@ public class InMemoryUserRepository implements UserRepository {
     @Override
     public List<User> getAll() {
         log.info("getAll");
-        return UsersUtil.sortUsersByName((List<User>)(repository.values()));
+        return new ArrayList<>(repository.values()).stream()
+                .sorted(Comparator.comparing(AbstractNamedEntity::getName)
+                        .thenComparing(AbstractBaseEntity::getId))
+                .collect(Collectors.toList());
     }
 
     @Override
