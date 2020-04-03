10e46d5d4538f7f4d428cee9224409c9305bd8a2
Kurururo <56359915+Kururuzo@users.noreply.github.com>
2020-04-03 21:21:10 +0300

    HW08 delete in Jsp changed on onClick

--- src/main/java/ru/javawebinar/topjava/web/meal/MealAjaxController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealAjaxController.java
@@ -1,13 +1,14 @@
 package ru.javawebinar.topjava.web.meal;
 
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.to.MealTo;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 @RestController
@@ -41,5 +42,14 @@ public class MealAjaxController extends AbstractMealController {
         }
     }
 
+    @Override
+    @GetMapping(value = "/filter", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<MealTo> getBetween(
+            @RequestParam LocalDate startDate,
+            @RequestParam LocalTime startTime,
+            @RequestParam LocalDate endDate,
+            @RequestParam LocalTime endTime) {
 
+        return super.getBetween(startDate, startTime, endDate, endTime);
+    }
 }
