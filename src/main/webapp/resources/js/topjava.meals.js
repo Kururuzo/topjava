fbde36d97588521be4dcd53135147d15ccf185d9
Kurururo <56359915+Kururuzo@users.noreply.github.com>
2020-04-15 22:24:52 +0300

    HW09 - added datetime-picker for meal filter

--- src/main/webapp/resources/js/topjava.meals.js
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -62,4 +62,45 @@ $(function () {
         }),
         updateTable: updateFilteredTable
     });
+
+    jQuery('#startDate').datetimepicker({
+        format:'Y-m-d',
+        onShow:function( ct ){
+            this.setOptions({
+                maxDate:jQuery('#endDate').val()?jQuery('#endDate').val():false
+            })
+        },
+        timepicker:false
+    });
+
+    jQuery('#endDate').datetimepicker({
+        format:'Y-m-d',
+        onShow:function( ct ){
+            this.setOptions({
+                minDate:jQuery('#startDate').val()?jQuery('#startDate').val():false
+            })
+        },
+        timepicker:false
+    });
+
+    jQuery('#startTime').datetimepicker({
+        format:'H:i',
+        onShow:function( ct ){
+            this.setOptions({
+                maxTime:jQuery('#endTime').val()?jQuery('#endTime').val():false
+            })
+        },
+        datepicker:false
+    });
+
+    jQuery('#endTime').datetimepicker({
+        format:'H:i',
+        onShow:function( ct ){
+            this.setOptions({
+                minTime:jQuery('#startTime').val()?jQuery('#startTime').val():false
+            })
+        },
+        datepicker:false
+    });
+
 });
